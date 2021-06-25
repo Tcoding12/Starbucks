758f8bee48d73897e4a23812bf216223b6c71ef4
Tino <anthony1@gmail.com>
2021-06-25 11:54:41 -0500

    new stuff

---
+++ script.js
@@ -0,0 +1,9 @@
+let myP = document.getElementById("tim");
+
+myP = addEventListener("click", stuff);
+
+function stuff() {
+  alert(
+    "Love the content bro, also i can't get into zoom.  i keep getting an error: 114something "
+  );
+}
