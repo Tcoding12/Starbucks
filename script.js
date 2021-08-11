19ed87441735490630895a43e23e2ddcb420ad34
Tino <anthony1@gmail.com>
2021-08-11 17:27:42 -0500

    stuff

--- script.js
+++
@@ -1,9 +0,0 @@
-let myP = document.getElementById("tim");
-
-myP = addEventListener("click", stuff);
-
-function stuff() {
-  alert(
-    "Love the content bro, i wannted to make it mobile responsive but i was out of time * I can't join the zoom meet*"
-  );
-}
